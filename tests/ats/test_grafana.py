415a8f57465d427fde24021c67a75f2ea0b8f5cd
QuantumEnigmaa <thibaud@giantswarm.io>
2024-07-31 14:05:13 +0200

    update ats tests

--- tests/ats/test_grafana.py
+++ tests/ats/test_grafana.py
@@ -1,16 +1,74 @@
-from typing import cast
+import logging
+import requests
+import time
+from contextlib import contextmanager
+from functools import partial
+from pathlib import Path
+from typing import Dict, List, Optional
 
 import pykube
 import pytest
 from pytest_helm_charts.clusters import Cluster
+from pytest_helm_charts.k8s.deployment import wait_for_deployments_to_run
+
+logger = logging.getLogger(__name__)
+
+namespace_name = "monitoring"
+deployment_name= "grafana"
+
+timeout: int = 560
+
+@pytest.mark.smoke
+def test_api_working(kube_cluster: Cluster) -> None:
+    """Very minimalistic example of using the [kube_cluster](pytest_helm_charts.fixtures.kube_cluster)
+    fixture to get an instance of [Cluster](pytest_helm_charts.clusters.Cluster) under test
+    and access its [kube_client](pytest_helm_charts.clusters.Cluster.kube_client) property
+    to get access to Kubernetes API of cluster under test.
+    Please refer to [pykube](https://pykube.readthedocs.io/en/latest/api/pykube.html) to get docs
+    for [HTTPClient](https://pykube.readthedocs.io/en/latest/api/pykube.html#pykube.http.HTTPClient).
+    """
+    assert kube_cluster.kube_client is not None
+    assert len(pykube.Node.objects(kube_cluster.kube_client)) >= 1
+
+# scope "module" means this is run only once, for the first test case requesting! It might be tricky
+# if you want to assert this multiple times
+@pytest.fixture(scope="module")
+def ic_deployment(request, kube_cluster: Cluster) -> List[pykube.Deployment]:
+    logger.info("Waiting for grafana deployment..")
+
+    deployment_ready = wait_for_ic_deployment(kube_cluster)
+
+    logger.info("grafana deployment looks satisfied..")
+
+    return deployment_ready
+
+def wait_for_ic_deployment(kube_cluster: Cluster) -> List[pykube.Deployment]:
+    deployments = wait_for_deployments_to_run(
+        kube_cluster.kube_client,
+        [deployment_name],
+        namespace_name,
+        timeout,
+    )
+    return deployments
 
 
-# when grafana pod is up, it might be still starting and returning 503
-@pytest.mark.flaky(reruns=10, reruns_delay=10)
-@pytest.mark.functional
-def test_grafana_login_page_available(kube_cluster: Cluster) -> None:
-    srv = cast(pykube.Service, pykube.Service.objects(kube_cluster.kube_client).get_or_none(name="grafana"))
-    if srv is None:
-        raise ValueError("'grafana service not found in the 'default' namespace")
-    login_page_res = srv.proxy_http_get("/login")
-    assert login_page_res.ok
+@pytest.fixture(scope="module")
+def pods(kube_cluster: Cluster) -> List[pykube.Pod]:
+    pods = pykube.Pod.objects(kube_cluster.kube_client)
+
+    pods = pods.filter(namespace=namespace_name, selector={
+                       'app.kubernetes.io/name': 'grafana', 'app.kubernetes.io/instance': 'grafana'})
+
+    return pods
+
+# when we start the tests on circleci, we have to wait for pods to be available, hence
+# this additional delay and retries
+
+
+@pytest.mark.smoke
+@pytest.mark.upgrade
+@pytest.mark.flaky(reruns=5, reruns_delay=10)
+def test_pods_available(kube_cluster: Cluster, ic_deployment: List[pykube.Deployment]):
+    for s in ic_deployment:
+        assert int(s.obj["status"]["readyReplicas"]) == int(
+            s.obj["spec"]["replicas"])
