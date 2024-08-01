eed72c1140e1d46817de743648bd560026058bdf
QuantumEnigmaa <thibaud@giantswarm.io>
2024-08-01 09:29:16 +0200

    nitpicking

--- tests/ats/test_grafana.py
+++ tests/ats/test_grafana.py
@@ -15,12 +15,8 @@ timeout: int = 560
 
 @pytest.mark.smoke
 def test_api_working(kube_cluster: Cluster) -> None:
-    """Very minimalistic example of using the [kube_cluster](pytest_helm_charts.fixtures.kube_cluster)
-    fixture to get an instance of [Cluster](pytest_helm_charts.clusters.Cluster) under test
-    and access its [kube_client](pytest_helm_charts.clusters.Cluster.kube_client) property
-    to get access to Kubernetes API of cluster under test.
-    Please refer to [pykube](https://pykube.readthedocs.io/en/latest/api/pykube.html) to get docs
-    for [HTTPClient](https://pykube.readthedocs.io/en/latest/api/pykube.html#pykube.http.HTTPClient).
+    """
+    Testing apiserver availability.
     """
     assert kube_cluster.kube_client is not None
     assert len(pykube.Node.objects(kube_cluster.kube_client)) >= 1
